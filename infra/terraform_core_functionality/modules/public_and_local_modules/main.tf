b87531bb901c31770e86686a6254a21931468000
Thomas Palma <66331874+thomaspalma1@users.noreply.github.com>
2026-02-05 16:58:32 -0300

    [fix] correct module sources and vpc variable name
    - fixed vpc module source to terraform-aws-modules/vpc/aws;
    - corrected private_subnets variable reference;
    - fixed ec2 module source to terraform-aws-modules/ec2-instance/aws.

--- infra/terraform_core_functionality/modules/public_and_local_modules/main.tf
+++ infra/terraform_core_functionality/modules/public_and_local_modules/main.tf
@@ -14,14 +14,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source  = "terraform-aws-module/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "4.0.2"
 
   name = var.vpc_name
   cidr = var.vpc_cidr
 
   azs             = var.vpc_azs
-  private_subnets = var.vpc_privatesubnets
+  private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
 
   enable_nat_gateway = var.vpc_enable_nat_gateway
@@ -31,7 +31,7 @@ module "vpc" {
 }
 
 module "ec2_instances" {
-  source  = "terraform-aws-modules/ec2-instances/aws"
+  source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.0.0"
 
   name  = "Cluster-A-${count.index}"
